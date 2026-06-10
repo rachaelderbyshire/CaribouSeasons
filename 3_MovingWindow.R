5ac00a2bacfefddc1e733f6d4c304e94441b9e2e
Rachael Derbyshire <rachaelderbyshire@trentu.ca>
2026-06-10 15:53:14 -0400

    Minor changes

--- 3_MovingWindow.R
+++ 3_MovingWindow.R
@@ -1,7 +1,7 @@
 #Use moving windows to characterize space use
 
 #code written by Rachael Derbyshire: rderbysh@uoguelph.ca
-#UPDATED May 2026
+#2025-2026
 
 #I would like to define season on an annual basis based on movement parameters and the space occupied by individuals
 #To do this, use fuzzy clustering methods, as described by Basille et al.
